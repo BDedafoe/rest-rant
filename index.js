93350ae100d8b7c445a68729bb3cdbf6c7f93c6c
BDedafoe <bmd6260@gmail.com>
2022-07-18 09:12:37 -0500

    adding 404 page view

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {            //homepage
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')       //404 page(routes not defined)
+    res.render('error404')       //404 page(routes not defined)
 })
 
 app.listen(process.env.PORT)
