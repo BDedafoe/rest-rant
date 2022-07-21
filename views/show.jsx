84dd75cd6e12aa079fb2034f01f20106f1c57a7c
BDedafoe <bmd6260@gmail.com>
2022-07-20 20:33:22 -0500

    Delete functionality implemented

--- views/show.jsx
+++ views/show.jsx
@@ -5,7 +5,29 @@ function show (data) {
     return (
         <Def>
             <main>
-                <h1>{data.place.name}</h1>
+                <div class="left-side">
+                    <image>{data.place.pic}</image>
+                </div>
+                
+                <div class="right-side">
+                    <h1>{data.place.name}</h1>
+                    <h2>Rating</h2>
+                    <h3>Not Rated</h3>
+                    
+                    <h2>Description</h2>
+                    <h3>Located in {data.place.city} and serving {data.place.cuisines}</h3>
+                        <a href="" className="btn btn-warning">Edit
+                        </a>  
+                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                        <button type="submit" className="btn btn-danger">Delete</button>
+                    </form>     
+
+                </div>
+               
+                <div>
+                    <h2>Comments</h2>
+                    <hh3>No comments yet!</hh3>
+                </div>
             </main>
         </Def>
     )
