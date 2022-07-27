38919a44198ef49644006af1eaf96a23ea304be1
BDedafoe <bmd6260@gmail.com>
2022-07-27 18:16:34 -0500

    fixed state selector

--- models/places.js
+++ models/places.js
@@ -5,7 +5,12 @@ const placeSchema = new mongoose.Schema({
   pic: String,
   cuisines: { type: String, required: true },
   city: { type: String, default: 'Anytown' },
-  state: { type: String, default: 'USA' },
+  state: { 
+    type: String, 
+    enum: ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Connecticut', 'Colorado', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Iowa', 'Idaho', 'Illinois', 'Indiana', 'Kentucky', 'Louisiana', 
+    'Massachussets', 'Maryland', 'Maine', 'Michigan', 'Minnesota', 'Missouri', 'Mississippi', 'Montana', 'North Carolina', 'North Dakota', 'Nebraska', 'New Hampshire', 'New Jersey', 'New Mexico', 'Nevada', 'New York', 
+    'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Virginia', 'Vermont', 'Washington', 'Wisconsin', 'West Virginia', 'Wyoming']
+  },
   founded: Number
 })
 
